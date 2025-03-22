265fb1636254ecf201881183bd57893338c2cb34
Amar Mandal <amarmandal2073@gmail.com>
2025-03-22 10:04:26 +0545

    fix: deleted_at and update_at edge case

--- linked-list/src/linked_list.rs
+++ linked-list/src/linked_list.rs
@@ -1,158 +1,165 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 pub struct Node<T> {
     data: T,
-    next: Option<Box<Node<T>>>,
+    next: Option<Rc<RefCell<Node<T>>>>,
 }
 
-// Define the LinkedList structure
 pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
+    head: Option<Rc<RefCell<Node<T>>>>,
 }
 
 impl<T> LinkedList<T> {
-    // Create a new empty linked list
     pub fn new() -> Self {
         LinkedList { head: None }
     }
 
-    // Add a new node to the front of the list
     pub fn push(&mut self, data: T) {
-        let new_node = Box::new(Node {
+        let new_node = Rc::new(RefCell::new(Node {
             data,
-            next: self.head.take(), // Take ownership of the current head
-        });
-        self.head = Some(new_node); // Set new node as head
+            next: self.head.take(), // Take ownership of current head
+        }));
+        self.head = Some(new_node);
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        let current_head = self.head.take();
+    pub fn pop(&mut self) -> Option<T>
+    where
+        T: Clone,
+    {
+        self.head.take().map(|node| {
+            self.head = node.borrow().next.clone(); // Clone next Rc
+            node.borrow().data.clone() // Clone data to return
+        })
+    }
 
-        match current_head {
-            Some(node) => {
-                self.head = node.next;
-                return Some(node.data);
-            }
-            None => {
-                return None;
-            }
+    pub fn reverse(&mut self) {
+        if self.head.is_none() {
+            return; // Empty list
         }
+        if self
+            .head
+            .as_ref()
+            .map_or(false, |n| n.borrow().next.is_none())
+        {
+            return; // Single node, no reverse needed
+        }
+
+        let mut prev: Option<Rc<RefCell<Node<T>>>> = None;
+        let mut current = self.head.clone();
+
+        while let Some(node) = current {
+            let next = node.borrow().next.clone();
+            node.borrow_mut().next = prev.clone();
+            prev = Some(node.clone());
+            current = next;
+        }
+
+        self.head = prev; // New head is the last node
     }
 
-    // fn reverse(&mut self) {
-    //     if self.head.is_none() {
-    //         println!("Empty List!");
-    //         return;
-    //     }
-
-    //     if self
-    //         .head
-    //         .as_ref()
-    //         .map_or(false, |n| n.borrow().next.is_none())
-    //     {
-    //         return;
-    //     }
-
-    //     let mut prev: Option<Rc<RefCell<Node>>> = None;
-    //     let mut current = self.head.clone();
-
-    //     while let Some(node) = current {
-    //         let next = node.borrow().next.as_ref().map(Rc::clone);
-    //         node.borrow_mut().next = prev.as_ref().map(Rc::clone);
-    //         prev = Some(node.clone());
-    //         current = next;
-    //     }
-
-    //     self.head = prev;
-    // }
-
-    pub fn insert_at(&mut self, data: T, index: usize)
+    pub fn insert_at(&mut self, data: T, pos: usize)
     where
         T: std::fmt::Display,
     {
-        let mut counter = 0;
+        let index = pos - 1;
+        let new_node = Rc::new(RefCell::new(Node { data, next: None }));
 
         if index == 0 || self.head.is_none() {
-            self.push(data);
+            new_node.borrow_mut().next = self.head.take();
+            self.head = Some(new_node);
             return;
         }
 
-        // condition for at least two nodes
-        let mut before_target_node = self.head.as_mut().unwrap();
+        let mut counter = 0;
+        let mut current = self.head.as_ref().map(Rc::clone);
 
-        while counter < index - 1 {
-            if let Some(ref mut next_node) = before_target_node.next {
-                before_target_node = next_node;
+        while let Some(node) = current {
+            if counter == index - 1 {
+                new_node.borrow_mut().next = node.borrow().next.clone();
+                node.borrow_mut().next = Some(Rc::clone(&new_node));
+                return;
             }
+            current = node.borrow().next.as_ref().map(Rc::clone);
             counter += 1;
         }
-
-        // at the position before target node
-        let new_node = Box::new(Node {
-            data,
-            next: before_target_node.next.take(),
-        });
-
-        before_target_node.next = Some(new_node);
     }
 
-    pub fn delete_at(&mut self, index: usize) -> Option<T> {
-        let mut counter = 0;
+    pub fn delete_at(&mut self, pos: usize) -> Option<T>
+    where
+        T: Clone,
+    {
+        let index = pos - 1;
 
+        // Empty list case
         if self.head.is_none() {
             return None;
         }
 
+        // Delete head (position 1)
         if index == 0 {
-            return self.pop();
+            let removed_node = self.head.take();
+            if let Some(node) = removed_node {
+                self.head = node.borrow().next.clone();
+                return Some(node.borrow().data.clone());
+            }
+            return None;
         }
 
-        // for 2 or more elements
-        let mut before_target_node = self.head.as_mut().unwrap();
+        // Traverse to find node before deletion point
+        let mut current = self.head.as_ref().map(Rc::clone);
+        let mut counter = 0;
 
-        while counter < index - 1 {
-            if let Some(ref mut next_node) = before_target_node.next {
-                before_target_node = next_node;
+        while let Some(node) = current {
+            if counter == index - 1 {
+                // Found node before the one to delete
+                let next_node = node.borrow().next.clone();
+                match next_node {
+                    Some(to_delete) => {
+                        // Skip the node to delete by connecting to the next one
+                        node.borrow_mut().next = to_delete.borrow().next.clone();
+                        return Some(to_delete.borrow().data.clone());
+                    }
+                    None => return None,
+                }
             }
+            current = node.borrow().next.as_ref().map(Rc::clone);
             counter += 1;
         }
 
-        if let Some(node_to_delete) = before_target_node.next.take() {
-            before_target_node.next = node_to_delete.next;
-            return Some(node_to_delete.data);
-        }
-
-        None
+        None // Index out of bounds
     }
 
     pub fn push_back(&mut self, data: T)
     where
-        T: std::fmt::Display + Copy,
+        T: std::fmt::Display,
     {
-        let new_node = Box::new(Node { data, next: None });
+        let new_node = Rc::new(RefCell::new(Node { data, next: None }));
 
         if self.head.is_none() {
             self.head = Some(new_node);
             return;
         }
 
-        let mut current = self.head.as_mut().unwrap();
+        let mut current = self.head.clone();
 
-        while let Some(ref mut next_node) = current.next {
-            current = next_node;
+        while let Some(node) = current {
+            if node.borrow().next.is_none() {
+                node.borrow_mut().next = Some(new_node);
+                return;
+            }
+            current = node.borrow().next.clone();
         }
-
-        // at this point this is the last none
-        current.next = Some(new_node);
     }
 
-    // Print the list (assuming T can be displayed)
     pub fn print(&self)
     where
         T: std::fmt::Display,
     {
-        let mut current = &self.head;
+        let mut current = self.head.clone();
         while let Some(node) = current {
-            print!("{} -> ", node.data);
-            current = &node.next;
+            print!("{} -> ", node.borrow().data);
+            current = node.borrow().next.clone();
         }
         println!("None");
     }
--- linked-list/src/main.rs
+++ linked-list/src/main.rs
@@ -1,20 +1,22 @@
 mod doubly_linked_list;
 mod linked_list;
 
-use doubly_linked_list::DoublyLinkedList;
-// use linked_list::LinkedList;
+// use doubly_linked_list::DoublyLinkedList;
+use linked_list::LinkedList;
 
 fn main() {
-    let mut list: DoublyLinkedList<i32> = DoublyLinkedList::new();
+    let mut list: LinkedList<i32> = LinkedList::new();
 
     list.push_back(10);
     list.push_back(20);
-    list.push_back(200);
-    list.push_back(2111);
+    list.push_back(30);
 
     list.print();
 
-    list.pop();
+    println!("Deleting the node at pos 2");
+    list.delete_at(2);
+
+    list.reverse();
 
     list.print();
 }
